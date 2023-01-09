3e30b1753e6c81b80c095328dbc68996b5adea50
Jiseok Shin <jshi0072@student.monash.edu>
2023-01-09 17:03:41 +1100

    passed reverse, passed remove(not final test)

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,18 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(array, ...args) {
+    let returnArray = array;
+    args.forEach((arg) => {
+        for(const item of array){
+            if(item == arg){
+                let index = array.indexOf(item);
+                if(index > -1){
+                    returnArray.splice(index,1)
+                }
+            }
+        }
+    }
+    )
+    
+    return returnArray
 };
 
 // Do not edit below this line
