b40e322596527c734e5c64e75158f36d2b53e224
Jiseok Shin <jshi0072@student.monash.edu>
2023-01-10 18:10:04 +1100

    passed

--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,47 @@
 const add = function() {
-	
+	let sum = 0;
+  for(num of arguments){
+    sum += num
+  }
+  return sum
 };
 
 const subtract = function() {
-	
+	return arguments[0] - arguments[1]
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+	let sum = 0;
+  for(num of array){
+    sum += num
+  }
+  return sum
 };
 
-const multiply = function() {
-
+const multiply = function(array) {
+  let product = 1;
+  for(num of array){
+    product *= num
+  }
+  return product
 };
 
 const power = function() {
-	
+	return Math.pow(arguments[0], arguments[1])
 };
 
 const factorial = function() {
-	
+  let num = arguments[0]
+  let result = 1;
+	if(num == 0){
+    return 1
+  }
+  else{
+    for(i=num; i>0; i--){
+      result *= i
+    }
+    return result
+  }
 };
 
 // Do not edit below this line
