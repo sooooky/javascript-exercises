282b9be812f4cd727e8c5799afaf3c957c156d7f
Jiseok Shin <jshi0072@student.monash.edu>
2023-01-10 17:15:16 +1100

    passed all testcases sumAll

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,18 @@
-const sumAll = function() {
-
+const sumAll = function(int1,int2) {
+    let sum = 0;
+    let largeInt = int2;
+    let smallInt = int1;
+    if(int1<0 || int2<0 || typeof(int1) !== "number" || typeof(int2) !== "number"){
+        return "ERROR"
+    }
+    if(int1 > int2){
+        largeInt = int1;
+        smallInt = int2;
+    }
+    for(i=smallInt; i<=largeInt; i++){
+        sum+=i
+    }
+    return sum;
 };
 
 // Do not edit below this line
