a13b2b210e5775d262a5094de9d992280c0adf7d
Jiseok Shin <jshi0072@student.monash.edu>
2023-01-11 15:48:44 +1100

    passed

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,18 @@
-const fibonacci = function() {
-
+const fibonacci = function(num) {
+    num = Number(num)
+    if(num<0){
+        return "OOPS"
+    }
+    let a = 0;
+    let b = 1;
+    let c = 1;
+    let result = 0;
+    for(i=1; i<num; i++){
+        c = a + b
+        a = b
+        b = c
+    }
+    return c
 };
 
 // Do not edit below this line
