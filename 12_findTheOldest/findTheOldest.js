d3032b48659090853af8077dca9a6eded999db4a
Jiseok Shin <jshi0072@student.monash.edu>
2023-01-12 15:22:07 +1100

    passed

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,6 +1,17 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(array) {
+    return array.reduce((oldest,current) => {
+        let oldestAge = findAge(oldest.yearOfBirth, oldest.yearOfDeath)
+        let currentAge = findAge(current.yearOfBirth, current.yearOfDeath)
+        return oldestAge > currentAge ? oldest:current
+    })
 };
+
+const findAge = function(birth,death){
+    if(!(death)){
+        death = new Date().getFullYear()
+    }
+    return death - birth
+}
 
 // Do not edit below this line
 module.exports = findTheOldest;
